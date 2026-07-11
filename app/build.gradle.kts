608120dca8b2abeaf2ae4a9a74fa608fee109c60
Dmytro Ponomarenko <dimmony@gmail.com>
2026-07-11 12:28:48 +0300

    Bump version to 2.2.1 and update version code to 946

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dimowner.audiorecorder"
         minSdk = 26
         targetSdk = 37
-        versionCode = 945
-        versionName = "2.2.0"
+        versionCode = 946
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
