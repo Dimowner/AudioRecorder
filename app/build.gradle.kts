1a8629c22739ff898cb569d210cad6c694a085d2
Dmytro Ponomarenko <dimmony@gmail.com>
2026-07-26 16:12:16 +0300

    Bump app version name and version code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dimowner.audiorecorder"
         minSdk = 26
         targetSdk = 37
-        versionCode = 946
-        versionName = "2.2.1"
+        versionCode = 947
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
